a31e5f521b9f876a0f184dde88c5fe690a62ef23
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-05-24 20:28:40 -0400

    created dev login and started devinputs page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import axios from 'axios';
 import Home from './Home';
 import DeveloperLogIn from './DeveloperLogIn';
-import Developer from './Developer';
+import DeveloperInput from './DeveloperInput';
 import DeveloperProfile from './DeveloperProfile';
 import StudentLogIn from './StudentLogIn';
-import Student from './Student';
+import StudentInput from './StudentInput';
 import StudentProfile from './StudentProfile';
 import LinkToStudents from './LinkToStudents';
+import Err from './Err';
 import base from './rebase';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
@@ -21,46 +24,59 @@ window.base = base;
 
 class App extends Component {
 
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     user: {},
-  //   }
-  // }
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+
+
+  githublogin (){
+    var authHandler = (error, data) => {
+      console.log('user', data.user)
+      this.setState({
+        user: data.user
+      })
+    }
+    //basic
+    base.authWithOAuthPopup('github', authHandler);
+  }
+
+
 
 
   render() {
+    console.log(this.state.user);
+    // let loggedin = this.state.user ? true : null
+    // console.log(loggedin);
     return (
       <Router>
         <div>
 
           <ul>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/developer">Developer</Link></li>
+            <li><Link to="/developerinput">DeveloperInput</Link></li>
             <li><Link to="/developerprofile">Developer Profile</Link></li>
-            <li><Link to="/student">Student</Link></li>
+            <li><Link to="/studentInput">Student Input</Link></li>
             <li><Link to="/studentprofile">Student Profile</Link></li>
             <li><Link to="/linktostudents">Link to Students</Link></li>
           </ul>
 
           <Route exact path="/" component={Home} />
-          <Route path="/developer" component={Developer} />
+          {/* <Route path="/developerinput" render={(pickles) => (loggedin ? <DeveloperInput logOut={this.logOut} /> : <Err />)} /> */}
           <Route path="/developerprofile" component={DeveloperProfile} />
-          <Route path="/student" component={Student} />
+          <Route path="/developerinput" component={DeveloperInput} />
+          <Route path="/studentinput" component={StudentInput} />
           <Route path="/studentprofile" component={StudentProfile} />
           <Route path="/linktostudents" component={LinkToStudents} />
           <Route path="/developerlogin" component={DeveloperLogIn} />
+          {/* <Route path="/developerlogin" render={(pickles) => (this.state.user ? (<Redirect to="/developerinput" />):(
+          <DeveloperLogIn githubLogin={this.githubLogin} logOut={this.logOut}{...pickles} /> ))} /> */}
           <Route path="/studentlogin" component={StudentLogIn} />
         </div>
       </Router>
-
-      // <div className="App">
-      //   <div className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h2>Welcome to React</h2>
-      //   </div>
-      //   <p>hello</p>
-      // </div>
+    
     );
   }
 }
--- src/DeveloperLogIn.js
+++ src/DeveloperLogIn.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import base from './rebase';
 
 // import axios from 'axios';
 
 class DeveloperLogIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+      base.auth().onAuthStateChanged(user => {
+        if (user) {
+          console.log('User is signed in.', user);
+          this.setState({
+            user: user
+          })
+         }
+      });
+    }
+
+  githubLogin (){
+    console.log('github')
+      var authHandler = (error, data) => {
+        console.log('user', data.user)
+        this.setState({
+          user: data.user
+        })
+      }
+      //basic
+      base.authWithOAuthPopup('github', authHandler);
+    }
+
+    logout () {
+      console.log('logout');
+    base.unauth()
+    this.setState({
+      user:{}
+    })
+  }
+
+  loginOrLogoutButton () {
+    if (this.state.user.uid) {
+      return <button onClick={this.logout.bind(this)}>Logout</button>
+    } else {
+      return <button onClick={this.githubLogin.bind(this)}>Login</button>
+    }
+  }
+
   render() {
+    // console.log(this.state.user);
     return (
       <div>
         <p>Dev login</p>
+        {/* <button onClick={this.githubLogin.bind(this)}>Sign in Github</button> */}
+        {this.loginOrLogoutButton()}
         <h1>Blah</h1>
       </div>
     )
