0d34cdeb1bd1b66ac17f3b5be3444a40f441b295
ddsheard <ddsheard@gmail.com>
2017-06-02 09:27:47 -0400

    update form

--- src/AddStuForm.js
+++ src/AddStuForm.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import StudentProfile from './StudentProfile';
+import { Redirect } from 'react-router-dom';
+import base from './rebase';
 
 
 class AddStuForm extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      user: []
+    }
+  }
+
   submitStuForm(event) {
     event.preventDefault();
     console.log("Page is good to move on");
@@ -12,20 +23,38 @@ class AddStuForm extends Component {
       goals: this.goals.value,
       tech: this.tech.value,
     }
+
     console.log(stu);
-    this.props.addStudentInput(stu);
-    this.stuForm.reset();
+    // this.props.addStudentInput(stu);
+    // this.stuForm.reset();
   }
 
+  let uid = this.props.user.uid
+  base.update(`user/${uid}`), {
+    data: {
+      availability: stu.availability,
+      objectives: stu.objectives,
+      experience: stu.expertise,
+      goals: stu.goals,
+      tech: stu.tech
+    }
+  })
+
+  this.setState({
+    submit: true
+  })
+
   render() {
+    if (this.state.submit === true) {
+      return (<Redirect to='/studentprofile' />)
+    }
     return(
-      <div>
         <div className="row">
           <div className="col s12 m6">
             <div className="card blue-grey darken-1">
             <div className="card-content white-text">
               <span className="card-title">Student Information Form</span>
-              <form ref={(input) => this.stuForm = input} className="stu-edit" onSubmit={(e) => this.submitStuForm(e)}>
+                <form ref={(input) => this.stuForm = input} className="stu-edit" onSubmit={(e) => this.submitStuForm(e)}>
                 <input ref={(input) => this.availability = input} type="text" placeholder="Student Availability" />
                 <div>
                   <textarea ref={(input) => this.objectives = input} type="text" className="materialize-textarea" placeholder="Student Objectives"></textarea>
@@ -45,19 +74,18 @@ class AddStuForm extends Component {
                   <option value="Java">Java</option>
                   <option value="PHP">PHP</option>
                   <option value="Python">Python</option>
-                  <option value="RoR">Ruby on Rails</option>
+                  <option value="Rails">Ruby on Rails</option>
                   </select>
                 </div>
                 <br />
 
-                  <button className="waves-effect waves-light btn" type="submit">Add Student Information</button>
+                  <button className="waves-effect waves-light btn" onClick={this.submitStuForm.bind(this)} type="submit">Add Student Information</button>
               </form>
 
             </div>
             </div>
           </div>
         </div>
-      </div>
     )
   }
 }
